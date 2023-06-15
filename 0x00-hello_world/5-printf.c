28c2a9102bf52fb970bdaf2dce86e8bb6bf06a64
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-15 10:54:50 -0700

    printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * prinf: is a function
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
