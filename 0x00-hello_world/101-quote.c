94f1fe231528f00a4191e5bf5455f363958b411a
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-15 13:15:47 -0700

    prints the task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+/**
+ * write - Progarm entry point
+ * main - Program entry point
+ * @argc: Number of argument
+ * @argv: Number of argument
+ *
+ * Return: 0 on success.
+ */
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main( int argc, char** argv )
+{
+   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 64);
+   return (0);
+}
