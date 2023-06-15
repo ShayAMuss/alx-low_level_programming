2bbe84d6b130af1db8cfbb8b39ac5efbb4221eb5
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-15 07:30:41 -0700

    func puts to print string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /*
- * main: Entry point
+ * main - Entry point
  * puts: is my function to print.
  *
  * return: 0.
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
