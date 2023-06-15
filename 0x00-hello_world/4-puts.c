fb97dcb57ddf0199520f91099deb205176ecf047
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-15 06:48:26 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main: where the function goes.
+ * puts: is my function to print.
+ * return: 0.
+ */
+int main(void);
+{
+	puts ("Programming is like building a multilingual puzzle\n");
+		return (0);
+}
