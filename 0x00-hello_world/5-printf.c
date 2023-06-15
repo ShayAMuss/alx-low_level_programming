c9f7151ecf34574c912beef4172e3900fc2e0982
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-15 11:06:24 -0700

    printing using func printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
+/**
  * main - program entry point
  * printf: is a function
  *
- * return: always 0
+ * return: always 0.
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
