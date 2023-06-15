d897d2fca9142f200b2207b44d31e9c1250edc84
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-15 07:43:47 -0700

    puts prints the string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /*
  * main - Entry point
- * puts: is my function to print.
- *
- * Description: prints exactly "Programming is like building a multilingual puzzle"
+ * 
+ * Description: prints exactly
  * return: 0.
  */
 int main(void)
