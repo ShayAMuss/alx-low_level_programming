cc083ea3c26c581baa7d786041170aae09380e4e
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-18 11:47:38 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * printf: Function to print digit numbers
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int i = 0;
+
+while (i <= 15)
+	{
+printf("%x", i);
+i++;
+	}
+	printf("\n");
+	return (0);
+}
