e0e748896ce74dd38aaa2667f522c01a60c3ea86
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-18 12:59:59 -0700

    possible comb of single dig

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,14 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
+	putchar(c);
 	if (c < '9')
-	{
-		putchar(c);
+{
 		putchar(',');
-		putchar(' ');
-	}
-	}
+
+}
+	putchar(' ');
+}
 putchar('\n');
 return (0);
 }
