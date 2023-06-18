a6bd5bbd86e679a04575a142ac5bfa7180e9ddca
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-18 07:41:53 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * printf: Function to print digit numbers
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
