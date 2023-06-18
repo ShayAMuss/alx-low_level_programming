fac313fbd57f70bb646d1c2abc2dcff064174f32
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-18 10:45:53 -0700

    Nuberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * putchar: Function to print alphabets
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int c = '0';
+
+	for (c <= '9'; c++)
+		putchar(c);
+putchar('\n');
+return (0);
+}
