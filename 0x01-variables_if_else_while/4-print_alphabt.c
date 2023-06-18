229e655d0cc2a2d1b88cba9ae0a8d6d4008cbbd7
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-18 06:45:43 -0700

    print alphatebts except q&e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * putchar: Function to print alphabets
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; && c != 'e'; && c != 'q'; c++)
+		putchar(c);
+putchar('\n');
+return (0);
+}
