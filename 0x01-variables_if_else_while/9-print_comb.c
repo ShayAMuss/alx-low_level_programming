a8084a5afff85e77c5efbcc44371c3dcbec5cb22
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-18 11:58:15 -0700

    possible comb of single dig

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * putchar: Function to print alphabets
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(', ');
+putchar('\n');
+return (0);
+}
