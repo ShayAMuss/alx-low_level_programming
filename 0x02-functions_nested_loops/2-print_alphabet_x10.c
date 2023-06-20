c619126c4bf4242ecb285f8d66a1a5591a9d67ec
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-20 04:41:52 -0700

    print alphabets lowercase x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,18 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * print_alphabet - Program entry point
+ * print_alphabet_x10 - Program entry point
  * _putchar: Function to print alphabets
  *
  * Return: 0 on success.
  */
- void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int c = 'a';
 
-for (;c <= 'z'; c++)
+for (; c <= 'z'; c++)
 {
 _putchar(c);
 }
-
 _putchar('\n');
 }
