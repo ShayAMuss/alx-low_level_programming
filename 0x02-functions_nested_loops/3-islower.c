cbf5c05e56f7fbdf76bbd9d56635d18d3321e712
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-20 01:46:23 -0700

    checking for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <main.h>
+/**
+ * main - Program entry point
+ * _islower: Function to check lowercase alphabets
+ *
+ * Return: 1 on success else 0.
+ */
+
+int _islower( int c )
+{
+
+
+for (c = '97'; c <= '122'; c++)
+{
+_islower(c);
+return (1);
+}
+if (c < '97' || c > '122')
+{
+return (0);
+}
+putchar('\n');
+}
