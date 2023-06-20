4b2fdcb89a79a46934dc514689149348a3265ffe
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-20 02:25:58 -0700

    putchar to print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,14 @@
  */
 int main (void)
 {
-	putchar('_putchar');
-	putchar('\n');
-	return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
