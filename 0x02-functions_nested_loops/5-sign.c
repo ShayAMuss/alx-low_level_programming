87e7b05ad47c5a33d74b372073e6b28e08457ca2
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-20 10:55:10 -0700

    checking sign type

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_sign - Program entry point
+ * _putchar: Function to print sign
+ * @n: is an int number
+ *
+ * Return: 1 on success else 0.
+ */
+
+int print_sign(int n)
+{
+
+if ( n > 0)
+{
+_putchar('+');
+return (1);
+}
+if ( n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+_putchar('\n');
+}
