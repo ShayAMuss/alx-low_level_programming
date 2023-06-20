92b44c4bd59b3b261d4de26e5bafdabae4968a8b
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-20 11:19:13 -0700

    abs to print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#enclude "main.h"
+#enclude <stdio.h>
+#enclude <stdlib.h>
+/**
+ * _abs - Program entry point
+ * _abs: Function to print absolute value
+ *
+ * Return: 0 on success.
+ */
+int _abs(int n)
+{
+
+_abs(n);
+putchar('\n');
+return (0);
+}
+
