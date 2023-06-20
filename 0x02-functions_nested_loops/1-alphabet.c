f4c9036c2efd5c7c0b540f8d8a3d8a0cb1f4d3e1
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-20 00:26:20 -0700

    alphabets by desired prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Program entry point
+ * _putchar: Function to print alphabets
+ *
+ * Return: 0 on success.
+ */
+ void print_alphabet(void)
+{
+int c = 'a';
+
+for (;c <= 'z'; c++)
+{
+_putchar(c);
+}
+
+_putchar('\n');
+return (0);
+}
