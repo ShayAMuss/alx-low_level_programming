c69f02f775a81c18853d4aab9190ad95df2a59a8
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-22 01:31:01 -0700

    print upto 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <limits.h>
+/**
+ * print_to_98 - Program entry point
+ * @n - is an int number
+ *
+ * Return: r on success.
+ */
+void print_to_98(int n)
+{
+
+if (n <= 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+if (n == 98)
+{
+printf("%d ", n);
+}
+}
+}
+else if (n >= 98)
+{
+while (n > 98)
+{
+printf("%d, ", n);
+--n;
+if (n == 98)
+{
+printf("%d ", n);
+}
+}
+}
+if (n == 98)
+{
+printf("%d ", n);
+}
+_putchar('\n');
+}
