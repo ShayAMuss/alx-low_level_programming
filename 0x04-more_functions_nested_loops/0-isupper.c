d9c2a018c2709a2801acd39cadf23f5e38c7a91b
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-23 06:29:18 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _isupper - Program entry poin.
+ * _isupper - Program entry point.
  * @c - is an int number.
  *
  * Return: 1 on success else 0.
@@ -9,6 +9,7 @@
 
 int _isupper(int c)
 {
+
 if (c == 'A')
 {
 return (1);
