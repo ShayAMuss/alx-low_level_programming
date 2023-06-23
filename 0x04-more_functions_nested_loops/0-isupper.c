2094a1d8e09141c60af7887011d159467059076c
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-23 05:11:09 -0700

    isupper and main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - Program entry poin.
+ * @c - is an int number.
+ *
+ * Return: 1 on success else 0.
+ */
+
+int _isupper(int c)
+{
+if (c = 'A' && c <= 'Z')
+{
+c++;
+return (1);
+}
+
+return (0);
+}
