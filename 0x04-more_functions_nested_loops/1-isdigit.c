3995c11bbe244ec04a7363ca191b3bd21b2c6237
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-23 06:56:06 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - Program entry poin.
+ * @c: is an int number.
+ *
+ * Return: always on success.
+ */
+
+int isdigit(int c)
+{
+if (c >='0' && c <= '9')
+{
+return (1);
+}
+return (0);
+}
