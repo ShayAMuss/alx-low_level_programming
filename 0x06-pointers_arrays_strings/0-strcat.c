0c98f45774131b2be7d664277666bc4299171b55
ShayAMuss <ahmedmusse2009@gmail.com>
2023-06-29 08:06:04 -0700

    main and str cocaatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
-* *strcat - is program to concatenate two strings
-* @*dest - is a pointer to char
-* @*src - is a pointer to char
+* _strcat - is program to concatenate two strings
+* @dest: is a pointer to char
+* @src: is a pointer to char
 *
 * Return: nothing.
 */
- 
+
 char *_strcat(char *dest, char *src)
 {
 int i;
@@ -22,5 +22,5 @@ dest[i + j] = src[i];
 
 dest[i + j] = '\0';
 
-return(0);
+return (0);
 }
