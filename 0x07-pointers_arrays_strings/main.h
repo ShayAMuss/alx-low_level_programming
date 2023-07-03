0ba2b27ef61a88504bcaaecafbb0473a604596d4
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-03 07:29:30 -0700

    0x07 ptr arr str

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+# char *_memset(char *s, char b, unsigned int n);
+# int _putchar(char c);
+
+#endif
