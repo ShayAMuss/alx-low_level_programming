ee5c124d8a392e02226f0acce8e92a94ce6808bd
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-03 08:02:34 -0700

    0x07 ptr arr str

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,16 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-void print_diagsums(int *a, int size);
-char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
-void simple_print_buffer(char *buffer, unsigned int size);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+
 #endif
