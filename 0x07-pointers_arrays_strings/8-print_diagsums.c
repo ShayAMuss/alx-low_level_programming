800b32245fe89104a02bc5d8e497f62965f9aa91
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-04 11:08:22 -0700

    0x07 ptr arr str

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,20 +12,20 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum1 = 0;
-	int sum2 =0;
+	int sum = 0;
+	int suM = 0;
 
 	for (i = 0; i < (size * size); i++)
 	{
 		if ( i % (size + 1) == 0)
 		{
-			sum1 += *(a + i);
+			sum += *(a + i);
 		}
-		else 
+		if (i % (size - 1) == 0 && i != 0)
 		{
-			sum2 += *(a + i);
+			suM += *(a + i);
 		}
 	}
 
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", sum, suM);
 }
