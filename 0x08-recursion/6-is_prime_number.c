35b1ff8140c9cf77b0ed281b5c4ddc4bc41cb9ad
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-08 07:30:24 -0700

    is prime number or not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,24 +11,16 @@
 
 int is_prime_number(int n)
 {
-int i, temp = 0;
+int i = 2;
 
-for (i = 2; i <= n / 2; i++)
-{
-if (n % i == 0)
-{
-temp++;
-break;
-}
-}
-
-if (temp == 0 && n >= 2)
-{
-return (1);
-}
-else
+if (n < 2)
 {
 return (0);
+}
+
+if (n % i != 0 && n % 5 != 0)
+{
+return (1);
 }
 
 return (0);
