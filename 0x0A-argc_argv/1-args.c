6a92c07d40805283ae88ccd54fc8ee3afcf7cc38
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-09 08:17:00 -0700

    count args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: success.
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+int i =  argc - 1;
+
+printf("%d\n", i);
+
+return (0);
+}
+
