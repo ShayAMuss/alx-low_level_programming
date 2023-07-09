a75322365966daba0207f4ca1a3a2846a9f2d435
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-09 07:08:52 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * @argc: argument count
+ * @argv - argument vector
+ *
+ * Return: success.
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+
+  printf("%s\n", argv[0]);
+
+  return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int main (__attribute__((unused)) int argc, char *argv[]);
+
+#endif
