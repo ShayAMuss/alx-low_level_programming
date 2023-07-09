745dbbc348b74d9764724041df5e156cf071cdb1
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-09 07:11:47 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
 /**
  * main - Program entry point
  * @argc: argument count
- * @argv - argument vector
+ * @argv: argument vector
  *
  * Return: success.
  */
@@ -12,7 +12,7 @@
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 
-  printf("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 
-  return (0);
+return (0);
 }
