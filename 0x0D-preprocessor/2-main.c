dede9691dbb646c47d0a81a244d299acb01ff50b
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-18 02:22:44 -0700

    from what is it compiled

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success.
+ */
+
+int main()
+{
+    printf("Name of source file: %s\n", __FILE__);
+
+    return (0);
+}
