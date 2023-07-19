12091942415902cc4a872aa40d87a062b21a976d
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-19 06:21:22 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - entry point
+ * @d: dog character
+ *
+ * Return: Nothing.
+ */
+void print_dog(struct dog *d)
+{
+if (d->name == NULL)
+{
+printf("Name: (nil))");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+
+{
+printf("Age: %f\n", d->age);
+}
+if (d->owner == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
