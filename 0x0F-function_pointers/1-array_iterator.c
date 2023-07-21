5c8da8adfc520043f81fb02c0653bbec696fbbf0
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-21 07:37:05 -0700

    function pointers 0F

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,12 +7,9 @@
  * Retrun: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
-		{
+{
 		size_t i;
 
-		if (array_iterator(int *array, size_t size, (*action)(int))
-				return;
-
-				for (i = 0; i < size; i++)
-				action(array[i]);
-				}
+for (i = 0; i < size; i++)
+	action(array[i]);
+}
