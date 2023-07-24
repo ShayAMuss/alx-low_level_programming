df92efba3f4c6364fd489951f24dee0c74f50fba
ShayAMuss <ahmedmusse2009@gmail.com>
2023-07-24 05:50:07 -0700

    print opcode of its main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,22 +3,38 @@
 
 /**
  * main - Prints opcodes of the main function
- * @argc: Amount og arguments
- * @argv: Value of the arguments
- * Return: Returns an int.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
+	int i, num;
+	
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
-			{
-			printf("Error\n");
-			exit(2);
-			}
-			
+
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("Error\n");
+                exit(2);
+	}
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%02hhx", ((char *)main)[i]);
+		if (i != num - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+
 	return (0);
 }
